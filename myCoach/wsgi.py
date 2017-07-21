7106b8def8d5fe300b82f517345eb46394d9af34
Nitin Mukesh <nitinmukesh681@gmail.com>
2017-07-21 15:33:41 +0530

    df

--- myCoach/wsgi.py
+++ myCoach/wsgi.py
@@ -13,8 +13,8 @@ from django.core.wsgi import get_wsgi_application
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-application = DjangoWhiteNoise(application)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myCoach.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
